6143542d209379826ae3984f4a2d3c96f039aae3
Andy <andymitchell1@live.co.uk>
2019-02-28 20:26:28 +0000

    first commit

---
+++ src/main/java/com/andymitchell/corsfilter/CorsFilterApplication.java
@@ -0,0 +1,13 @@
+package com.andymitchell.corsfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorsFilterApplication.class, args);
+    }
+
+}
